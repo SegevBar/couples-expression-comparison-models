cc87d4bcd1db715dc8aaa9be18d7ab39b1dd0ba6
Bar Segev <segevbar94@gmail.com>
2023-09-19 00:15:25 +0300

    reqirement emoca file

--- EMOCA/emoca_installation.sh
+++ EMOCA/emoca_installation.sh
@@ -22,25 +22,21 @@ fi
 echo "Installing conda packages"
 mamba env update -n work38 --file emoca_model/conda-environment_py38_cu11_ubuntu.yml
 echo "Installing other requirements"
-pip install -r emoca_model/requirements38.txt
+pip install -r emoca_model/requirements.txt
 pip install Cython==0.29
 
 eval "$(conda shell.bash hook)"
 conda activate work38
 
-pip install pandas==1.4.2
-pip install numba==0.56.4
-pip install numpy==1.20.3
-pip install scikit-video==1.1.11
-pip install ipykernel
-pip install ffmpeg-python
-
-echo "Making sure Pytorch3D installed correctly"
-pip install git+https://github.com/facebookresearch/pytorch3d.git@v0.6.2
+#pip install pandas==1.4.2
+#pip install numba==0.56.4
+#pip install numpy==1.20.3
+#pip install scikit-video==1.1.11
+#pip install ipykernel
+#pip install ffmpeg-python
+#
+#echo "Making sure Pytorch3D installed correctly"
+#pip install git+https://github.com/facebookresearch/pytorch3d.git@v0.6.2
 echo "Installing GDL"
 pip install -e .
 echo "Installation finished"
-
-echo "install models"
-cd emoca_model/gdl_apps/EMOCA/demos
-bash emoca_model/download_assets.sh
