31acf97468bb54ec3cb26e8dd411751b9f6c9567
Bar Segev <segevbar94@gmail.com>
2023-09-21 22:01:37 +0300

    update spectre installation

--- Epression_Represantation_Generators/SPECTRE/spectre_installation.sh
+++ Epression_Represantation_Generators/SPECTRE/spectre_installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating conda environment"
-mamba create -n spectre python=3.8
+conda create --name spectre python=3.8
 eval "$(conda shell.bash hook)" # make sure conda works in the shell script
 conda activate spectre
 if echo $CONDA_PREFIX | grep spectre
@@ -12,12 +12,11 @@ else
     exit
 fi
 echo "Installing conda packages"
-pip install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
-pip install -c conda-forge -c fvcore fvcore iopath
+conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
+conda install -c conda-forge -c fvcore fvcore iopath
 pip install -r spectre_model/requirements.txt
 pip install -c conda-forge yacs
 pip install numpy==1.23.1
-pip install git+https://github.com/facebookresearch/pytorch3d.git@v0.6.2
 
 echo "Installing external packages"
 cd spectre/external/face_alignment
