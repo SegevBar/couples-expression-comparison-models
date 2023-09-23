9e568114f42220fb640e2449715737c4364c78c5
Bar Segev <segevbar94@gmail.com>
2023-09-23 15:08:27 +0300

    deca 2d

--- Expression_Represantation_Generators/DECA/deca_model/fetch_data.sh
+++ Expression_Represantation_Generators/DECA/deca_model/fetch_data.sh
@@ -11,13 +11,13 @@ password=$(urle $password)
 mkdir -p ./data
 
 echo -e "\nDownloading FLAME..."
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=flame&sfile=FLAME2020.zip&resume=1' -O './data/FLAME2020.zip' --no-check-certificate --continue
-unzip ./data/FLAME2020.zip -d ./data/FLAME2020
-mv ./data/FLAME2020/generic_model.pkl ./data
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=flame&sfile=FLAME2020.zip&resume=1' -O './deca_model/data/FLAME2020.zip' --no-check-certificate --continue
+unzip ./deca_model/data/FLAME2020.zip -d ./deca_model/data/FLAME2020
+mv ./deca_model/data/FLAME2020/generic_model.pkl ./deca_model/data
 # rm -rf ./models
 
 echo -e "\nDownloading deca_model..."
 
 FILEID=1rp8kdyLPvErw2dTmqtjISRVvQLj6Yzje
-FILENAME=./data/deca_model.tar
+FILENAME=./deca_model/data/deca_model.tar
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='${FILEID} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O $FILENAME && rm -rf /tmp/cookies.txt
